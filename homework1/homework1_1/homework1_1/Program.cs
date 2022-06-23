46763c94180649b353fd5b7c6f56fd8c83ae1828
桢 王 <3438326503@qq.com>
2022-06-24 07:25:27 +0800

    homework1

---
+++ homework1/homework1_1/homework1_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace homework1_1
+{
+    class Program
+    {
+       static void Main(string[] args)
+            {
+                double a, b;
+                char opertate;
+
+                Console.WriteLine("请输入第一个运算数：");
+                string str1 = Console.ReadLine();
+                a = Convert.ToDouble(str1);
+
+                Console.WriteLine("请输入运算符号：");
+                string str2 = Console.ReadLine();
+                opertate = Convert.ToChar(str2);
+
+                Console.WriteLine("请输入第二个运算数：");
+                string str3 = Console.ReadLine();
+                b = Convert.ToDouble(str3);
+
+                switch (opertate)
+                {
+                    case '+':
+                        Console.WriteLine($"{a} + {b}={a + b}");
+                        break;
+                    case '-':
+                        Console.WriteLine($"{a} - {b}={a - b}");
+                        break;
+                    case '*':
+                        Console.WriteLine($"{a} * {b}={a * b}");
+                        break;
+                    case '/':
+                        Console.WriteLine($"{a} / {b}={a / b}");
+                        break;
+                    case '%':
+                        Console.WriteLine($"{a} % {b}={a % b}");
+                        break;
+                    default:
+                        Console.WriteLine("运算符不正确"); break;
+                }
+            }
+     
+    }
+}
---
+++ homework1/homework1_2/homework1_2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double a, b;
+            char operate;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            operate = Convert.ToChar(comboBox1.SelectedItem.ToString());
+            switch (operate)
+            {
+                case '+':
+                    label1.Text = (a + b).ToString();
+                    break;
+                case '-':
+                    label1.Text = (a - b).ToString();
+                    break;
+                case '*':
+                    label1.Text = (a * b).ToString();
+                    break;
+                case '/':
+                    label1.Text = (a / b).ToString();
+                    break;
+                case '%':
+                    label1.Text = (a % b).ToString();
+                    break;
+            }
+        }
+    }
+}
